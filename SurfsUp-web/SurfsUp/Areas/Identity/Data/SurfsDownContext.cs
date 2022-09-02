398fb98f462729939474bf44983a88f8f45b9b15
Jens Lauritsen <jens.bech.lauritsen@gmail.com>
2022-09-02 10:12:02 +0200

    Authentication and Authorization

---
+++ SurfsUp-web/SurfsUp/Areas/Identity/Data/SurfsDownContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SurfsUp.Areas.Identity.Data;
+using SurfsUp.Models;
+
+namespace SurfsUp.Data;
+
+public class SurfsUpContext : IdentityDbContext<SurfsUpUser>
+{
+    public SurfsUpContext(DbContextOptions<SurfsUpContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<SurfsUp.Models.Surfboard>? Surfboard { get; set; }
+}
---
+++ SurfsUp-web/SurfsUp/Controllers/SurfboardsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SurfsUp.Data;
+using SurfsUp.Models;
+
+namespace SurfsUp.Controllers
+{
+    public class SurfboardsController : Controller
+    {
+        private readonly SurfsUpContext _context;
+
+        public SurfboardsController(SurfsUpContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Surfboards
+        public async Task<IActionResult> Index()
+        {
+              return _context.Surfboard != null ? 
+                          View(await _context.Surfboard.ToListAsync()) :
+                          Problem("Entity set 'SurfsUpContext.Surfboard'  is null.");
+        }
+
+
+        
+        // GET: Surfboards/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Surfboard == null)
+            {
+                return NotFound();
+            }
+
+            var surfboard = await _context.Surfboard
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (surfboard == null)
+            {
+                return NotFound();
+            }
+
+            return View(surfboard);
+        }
+
+        // GET: Surfboards/Create
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Surfboards/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Create([Bind("Image,Id,Title,Length,Width,Volume,Thickness,Type,Price,Equipment,RentedOut")] Surfboard surfboard)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(surfboard);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(surfboard);
+        }
+
+        // GET: Surfboards/Edit/5
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Surfboard == null)
+            {
+                return NotFound();
+            }
+
+            var surfboard = await _context.Surfboard.FindAsync(id);
+            if (surfboard == null)
+            {
+                return NotFound();
+            }
+            return View(surfboard);
+        }
+
+        // POST: Surfboards/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Edit(int id, [Bind("Image,Id,Title,Length,Width,Volume,Thickness,Type,Price,Equipment,RentedOut")] Surfboard surfboard)
+        {
+            if (id != surfboard.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(surfboard);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SurfboardExists(surfboard.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(surfboard);
+        }
+
+        // GET: Surfboards/Delete/5
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Surfboard == null)
+            {
+                return NotFound();
+            }
+
+            var surfboard = await _context.Surfboard
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (surfboard == null)
+            {
+                return NotFound();
+            }
+
+            return View(surfboard);
+        }
+
+        // POST: Surfboards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Surfboard == null)
+            {
+                return Problem("Entity set 'SurfsUpContext.Surfboard'  is null.");
+            }
+            var surfboard = await _context.Surfboard.FindAsync(id);
+            if (surfboard != null)
+            {
+                _context.Surfboard.Remove(surfboard);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SurfboardExists(int id)
+        {
+          return (_context.Surfboard?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
