6f4381d2b94a3a06bb5bed1da186101ccd74df39
Jens Lauritsen <jens.bech.lauritsen@gmail.com>
2022-09-09 11:34:14 +0200

    UsersList ^-^

---
+++ SurfsUp-web/Controllers/ManageUsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SurfsUp.Areas.Identity.Data;
+using System.Data;
+
+namespace SurfsUp.Controllers
+{
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<SurfsUpUser> userManager;
+      
+        public ManageUsersController(UserManager<SurfsUpUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet]
+        public IActionResult ListUsers()
+        {
+            var users = userManager.Users;
+            return View(users.ToList());
+        }
+    }
+}
