6629b2bca1c4cf33195a6684e30275d79fd77cb1
LovelessCodes <hello@loveless.codes>
2022-09-13 11:04:10 +0200

    "make admin" knap virker :o)

--- SurfsUp-web/Controllers/ManageUsersController.cs
+++ SurfsUp-web/Controllers/ManageUsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SurfsUp.Areas.Identity.Data;
+using System.Collections.Generic;
 using System.Data;
 
 namespace SurfsUp.Controllers
@@ -16,18 +17,54 @@ namespace SurfsUp.Controllers
             this.userManager = userManager;
         }
 
-       
-
         [Authorize(Roles = "Administrator")]
         [HttpGet]
-        public IActionResult ListUsers()
+        public async Task<IActionResult> ListUsers()
         {
             var users = userManager.Users;
-            return View(users.ToList());
+            var admins = await userManager.GetUsersInRoleAsync("Admin");
+            List<List<SurfsUpUser>> model = new()
+            {
+                users.ToList(), admins.ToList()
+            };
+            return View("ListUsers", model);
         }
 
 
-      
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> ToggleAdmin(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            var userRoles = await userManager.GetRolesAsync(user);
+            IdentityResult result = new IdentityResult();
+            if (userRoles.Contains("Administrator"))
+            {
+                result = await userManager.RemoveFromRoleAsync(user, "Admin");
+            }
+            else
+            {
+                result = await userManager.AddToRoleAsync(user, "Admin");
+            }
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ListUsers");
+            }
+            foreach (var error in result.Errors)
+            {
+                Console.WriteLine(error.Description);
+                ModelState.AddModelError("", error.Description);
+            }
+            return RedirectToAction("ListUsers");
+        }
+
+
+        [Authorize(Roles = "Administrator")]
         [HttpPost]
         public async Task<IActionResult> DeleteUser(string id)
         {
