594cb5b8a1246800f63e0f775ef43a9e042d6335
Jens Lauritsen <evertonstyrer@gmail.com>
2022-09-29 10:16:36 +0200

    Reversing idk

---
+++ SurfsUp-web/Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿using SurfsUp.Areas.Identity.Data;
+
+namespace SurfsUp.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime BookingDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public Surfboard? Surfboard { get; set; }
+        public SurfsUpUser? User { get; set; }
+    }
+}
