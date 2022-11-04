45d1a4aeb07fbbe29937ccc062b1775f748650f2
Jens Lauritsen <evertonstyrer@gmail.com>
2022-11-04 11:04:47 +0100

    SurfboardCard

--- SurfsUp-API/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ SurfsUp-API/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,13 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using SurfsUp_Models;
 
 namespace SurfsUp_API.Areas.Identity.Pages.Account
--- SurfsUp-Blazor/Program.cs
+++ SurfsUp-Blazor/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7211") });
 
 await builder.Build().RunAsync();
