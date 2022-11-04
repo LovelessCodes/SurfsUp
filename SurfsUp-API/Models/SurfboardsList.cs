ade5ed45b5160a235b2a020c8232a9ac2932a1f9
LovelessCodes <hello@loveless.codes>
2022-11-04 09:39:41 +0100

    removed models from api

--- SurfsUp-API/Models/SurfboardsList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SurfsUp_Models
-{
-    public class SurfboardsList
-    {
-        public List<Surfboard> Surfboards { get; set; }
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-    }
-}
