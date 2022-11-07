95bdb6272adb0b250403cbe45735f9c0594d1dcc
Jens Lauritsen <jbla38240@edu.ucl.dk>
2022-11-07 16:45:08 +0100

    Added Surfboards NavMenu link

---
+++ SurfsUp-web/Migrations/20221013124737_RowVersion.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurfsUp.Migrations
+{
+    public partial class RowVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_AspNetUsers_UserId",
+                table: "Booking");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Surfboard_SurfboardId",
+                table: "Booking");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Booking_SurfboardId",
+                table: "Booking");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Booking_UserId",
+                table: "Booking");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Surfboard",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Booking",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Surfboard");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Booking",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Booking_SurfboardId",
+                table: "Booking",
+                column: "SurfboardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Booking_UserId",
+                table: "Booking",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_AspNetUsers_UserId",
+                table: "Booking",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Surfboard_SurfboardId",
+                table: "Booking",
+                column: "SurfboardId",
+                principalTable: "Surfboard",
+                principalColumn: "Id");
+        }
+    }
+}
