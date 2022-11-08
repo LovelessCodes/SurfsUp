be274ce32695f5f516150f1187eacf0b2ce9bf0d
LovelessCodes <hello@loveless.codes>
2022-11-08 12:49:40 +0100

    style master 2022

--- SurfsUp-web/Migrations/20221013124737_RowVersion.Designer.cs
+++ SurfsUp-web/Migrations/20221013124737_RowVersion.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using SurfsUp.Data;
+using SurfsUp_Models;
 
 #nullable disable
 
